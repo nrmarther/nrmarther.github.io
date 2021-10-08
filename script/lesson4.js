926406e88ccb16023634037779a4b961245aadb3
nrmarther <nathan@martherus.com>
2021-10-08 12:26:13 -0600

    finally fixed mobile menu and last updated

--- script/lesson4.js
+++ script/lesson4.js
@@ -1,9 +1,30 @@
+//menu bar navigation
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
 // footer year and last updated
-document.getElementById('modified').innerHTML = document.lastModified;
-let date = new Date();
-document.getElementById('year').innerHTML = date.getUTCFullYear();
+let d = new Date();
+document.getElementById('year').innerHTML = d.getUTCFullYear();
+
+
+let months = ["January", "February", "March", "April", "May",
+              "June", "July", "August", "September", "October",
+              "November", "December"];
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+            "Thursday", "Friday", "Saturday"];
+
+
+let day = days[d.getDay()];
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+let lastUpdated = `${day}, ${d.getDate()} ${month} ${year}`;
+
+document.getElementById('modified').innerHTML = lastUpdated;

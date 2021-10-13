f85cff8a1a8009c115ab6bd05cb64062706df89a
nrmarther <nathan@martherus.com>
2021-10-13 15:34:39 -0600

    added stuff for week 5 including banner and stuff

--- bom/bom.js
+++ bom/bom.js
@@ -1,14 +1,21 @@
+var list = document.getElementById("list");
+var node = document.createElement("li");
+var rawInput = document.getElementById("favchap").value;
+var input = document.createTextNode(rawInput);
+var delButton = document.createElement("button");
+delButton.innerHTML = "Delete";
 
+function addItem() {
+    console.log("raw input = " + rawInput);
+    if (input == " ") {
+        console.log("please input your favorite Book of Mormon Scripture");
+    }
+    else {
+        console.log("raw input = " + rawInput);
+        list.appendChild(node);
+        node.appendChild(input);
+        node.appendChild(delButton);
+    }
+}
 
-var input = document.createTextNode(document.getElementById("favchap"));
-var node = document.createElement("LI");
-var butt = document.createElement("BUTTON");
-var fullButt = butt.appendChild(document.createTextNode("Delete"));
-var list = document.getElementsByClassName("list");
-
-
-function addItem(input) {
-    list.appendChild(node);
-    node.appendChild(input);
-    node.appendChild(fullButt);
-};
+function addItem();
--- script/lesson4.js
+++ script/lesson4.js
@@ -4,8 +4,43 @@ const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+
+
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+
+//do not show the 5 day forecast in mobile view
+var media = window.matchMedia("(max-width: 37.4em)");
+
+function del5Day(media) {
+    if (media.matches) {
+        document.getElementsByClassName("content_box")[1].style.display = "none";
+    }
+    else {
+        document.getElementsByClassName("content_box")[1].style.display = "block";
+    }
+}
+
+del5Day(media);
+media.addListener(del5Day);
+
+
+
+//show pancake's in park banner on friday's
+var date = new Date();
+var day = date.getDay();
+function banner() {
+    if (day == 5) {
+        document.getElementById("banner").style.display = "block";
+    }
+    else {
+        document.getElementById("banner").style.display = "none";
+    }
+}
+banner();
+
 
 
 // footer year and last updated
@@ -21,7 +56,7 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday"];
 
 
-let day = days[d.getDay()];
+day = days[d.getDay()];
 let month = months[d.getMonth()];
 let year = d.getFullYear();
 
@@ -42,4 +77,4 @@ function lastSave() {
     console.log(isoString);
     const date = new Date(isoString);
     const upDate = new Intl.DateTimeFormat("en-US", options)
-}
+};

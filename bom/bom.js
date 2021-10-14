870ade89f88631bf64b2d7a5eef56f9ac1778917
nrmarther <nathan@martherus.com>
2021-10-13 21:51:08 -0600

    starting to fix bom assignment

--- bom/bom.js
+++ bom/bom.js
@@ -1,21 +1,30 @@
-var list = document.getElementById("list");
-var node = document.createElement("li");
-var rawInput = document.getElementById("favchap").value;
-var input = document.createTextNode(rawInput);
-var delButton = document.createElement("button");
-delButton.innerHTML = "Delete";
+// var list = document.getElementById("list");
+// var node = document.createElement("li");
+// var rawInput = document.getElementById("favchap").value;
+// var input = document.createTextNode(rawInput);
+// var delButton = document.createElement("button");
+// delButton.innerHTML = "Delete";
 
 function addItem() {
-    console.log("raw input = " + rawInput);
+    var list = document.getElementById("list");
+    var node = document.createElement("li");
+    var rawInput = document.getElementById("favchap").value;
+    var input = document.createTextNode(rawInput);
+    var delButton = document.createElement("button");
+    delButton.innerHTML = "Delete";
+    delButton.className = "delButton";
+    input.className= "favScrip"
+
     if (input == " ") {
         console.log("please input your favorite Book of Mormon Scripture");
     }
     else {
-        console.log("raw input = " + rawInput);
         list.appendChild(node);
         node.appendChild(input);
         node.appendChild(delButton);
     }
-}
-
-function addItem();
+};
+var delButton = document.getElementsByClassName("delButton");
+var input = document.getElementsByClassName("favScrip");
+delButton.onclick='input.remove()';
+delButton.onclick='delButton.remove()';

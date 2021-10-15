20c45d108c7c73526eaed405e6f2d852990ddade
nrmarther <nathan@martherus.com>
2021-10-14 19:06:19 -0600

    added a last updated function to the footer

--- bom/bom.js
+++ bom/bom.js
@@ -24,7 +24,31 @@ function addItem() {
         node.appendChild(delButton);
     }
 };
+
+//delete button 
 var delButton = document.getElementsByClassName("delButton");
 var input = document.getElementsByClassName("favScrip");
 delButton.onclick='input.remove()';
 delButton.onclick='delButton.remove()';
+
+
+
+
+//last updated function
+let d = new Date(document.lastModified);
+
+let months = ["January", "February", "March", "April", "May",
+              "June", "July", "August", "September", "October",
+              "November", "December"];
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+            "Thursday", "Friday", "Saturday"];
+
+
+day = days[d.getDay()];
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+let lastUpdated = `${day}, ${d.getDate()} ${month} ${year}`;
+
+document.getElementById('modified').innerHTML = lastUpdated;

38096620faf47093fcf53d2601595c0dc719695d
nrmarther <nathan@martherus.com>
2021-10-20 15:32:09 -0600

    fixed windchill and formatting for hero img

--- script/lesson4.js
+++ script/lesson4.js
@@ -46,7 +46,7 @@ function lastSave() {
     const upDate = new Intl.DateTimeFormat("en-US", options)
 };
 
-//---------------------------------PRESTON PAGE------------------------------
+//---------------------------------PRESTON PAGE-------------------------------
 
 //do not show the 5 day forecast in mobile view
 var media = window.matchMedia("(max-width: 37.4em)");
@@ -64,7 +64,7 @@ media.addListener(del5Day);
 
 
 
-//show pancake's in park banner on friday's
+// ------------------ show pancake's in park banner on friday's ---------------
 var date = new Date();
 var day = date.getDay();
 function banner() {
@@ -78,7 +78,29 @@ function banner() {
 banner();
 
 
-// Calculate the Windchill
-function buildWC(speed, temp) {
+// -------------------- Calculate the Windchill -------------------------------
+let temp = document.querySelector('curTemp');
+let speed = document.querySelector('curSpeed');
 
-}
+buildWC(speed, temp);
+
+function buildWC(speed, temp) {
+    let wcTemp = document.getElementById('windchill');
+   
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log('windchill 01: ' + wc);
+   
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+    console.log('windchill 02: ' + wc);
+   
+    // If chill is greater than temp, return the temp
+    // wc = (wc > temp)?temp:wc;
+    console.log('windchill 03: ' + wc);
+   
+    // Display the windchill
+    console.log('windchill 04: ' + wc);
+    wc = wc+'°F';
+    wcTemp.innerHTML = wc;
+    }

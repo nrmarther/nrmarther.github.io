4b6706a827891100d5c9594cf0e7dbf8d2d4d5c0
nrmarther <nathan@martherus.com>
2021-10-27 15:33:08 -0600

    added descriptions and lazy loading

--- script/gallery.js
+++ script/gallery.js
@@ -45,3 +45,35 @@ function lastSave() {
     const date = new Date(isoString);
     const upDate = new Intl.DateTimeFormat("en-US", options)
 };
+
+//lazy load images
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 0.05
+}
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}

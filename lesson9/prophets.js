ac48df6766dd7146df6fdc67017a44def10ff456
nrmarther <nathan@martherus.com>
2021-11-10 10:49:04 -0700

    reorganized weathersite

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -21,10 +21,7 @@ for (let i = 0; i < prophets.length; i++ ) {
     let h2 = document.createElement('h2');
 
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
     card.appendChild(h2);
-
     document.querySelector('div.cards').appendChild(card);
-
     image.setAttribute('src', prophets[i].imageurl);
 }

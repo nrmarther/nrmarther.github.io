501c44e9bea78a66a31adce6df7e448935eefe93
nrmarther <nathan@martherus.com>
2021-12-07 22:35:39 -0700

    making changes to gilbert page

---
+++ gilbert/script/gilbert.js
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------- GENERAL ----------------------------------------------------------------
+//menu bar navigation
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+// footer year and last updated
+let d = new Date(document.lastModified);
+document.getElementById('year').innerHTML = d.getUTCFullYear();
+
+
+const months = ["January", "February", "March", "April", "May",
+              "June", "July", "August", "September", "October",
+              "November", "December"];
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+            "Thursday", "Friday", "Saturday"];
+
+
+day = days[d.getDay()];
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+let lastUpdated = `${day}, ${d.getDate()} ${month} ${year}`;
+
+document.getElementById('modified').innerHTML = lastUpdated;
